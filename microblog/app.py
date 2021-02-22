07350120ca428de5956a9b50e69567b74c5ea609
vudinhquang <vudinhquangk53@gmail.com>
2021-02-23 04:40:53 +0900

    Retrieving the Microblog entries from MongoDB

--- microblog/app.py
+++ microblog/app.py
@@ -16,25 +16,22 @@ class GalileanMoons:
         self.first=first
         self.second=second
         self.third=third
-        self.fourth=fourth
-
-entries = []       
+        self.fourth=fourth 
     
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((entry_content, formatted_date))
         app.db.entries.insert({"content": entry_content, "date": formatted_date})
 
     entries_with_date = [
         (
-            entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
         )
-        for entry in entries
+        for entry in app.db.entries.find({})
     ]
 
     return render_template("home.html", entries=entries_with_date)
