8fe0348984ba38e2155f380c064e05a7be04e544
vudinhquang <vudinhquangk53@gmail.com>
2021-02-22 18:16:12 +0900

    Jinja2 Expressions

--- microblog/app.py
+++ microblog/app.py
@@ -22,3 +22,34 @@ def jinja_intro():
     return render_template(
             "jinja_intro.html", name="John", template_name="Jinja2"
         )
+
+@app.route("/expressions")
+def expressions():
+    # interpolation
+    color = "brown"
+    animal_one = "fox"
+    animal_two = "dog"
+    
+    # addition and subtraction
+    orange_amount = 10
+    apple_amount = 20
+    donate_amount = 15
+
+    # string concatenation
+    first_name = "Caption"
+    last_name = "Marvel"
+
+    kwargs = {
+        "color": color,
+        "animal_one": animal_one,
+        "animal_two": animal_two,
+        "orange_amount": orange_amount,
+        "apple_amount": apple_amount,
+        "donate_amount": donate_amount,
+        "first_name": first_name,
+        "last_name": last_name,
+    }
+
+    return render_template(
+            "expressions.html", **kwargs
+        )
