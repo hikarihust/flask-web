8d90869cf39a99cfa17aceb1904786edb186678c
vudinhquang <vudinhquangk53@gmail.com>
2021-02-23 22:09:03 +0900

    Creating a project that uses MongoDB and pymongo

---
+++ terminal_blog/app.py
@@ -0,0 +1,13 @@
+import pymongo
+
+URI = "mongodb://127.0.0.1:27017"
+client = pymongo.MongoClient(URI)
+database = client['fullstack']
+collection = database['students']
+
+students = collection.find({})
+
+print(students)
+for student in students:
+    print(student)
+    
