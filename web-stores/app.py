06c09faf20844fb6741ae7c18f2d7bf38680f73c
vudinhquang <vudinhquangk53@gmail.com>
2021-02-25 02:26:40 +0900

    Getting an element's content with BeautifulSoup

--- web-stores/app.py
+++ web-stores/app.py
@@ -1,5 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
 
-response = requests.get("https://www.google.com/")
-print(response.content)
+URL = "https://www.johnlewis.com/ruark-mrx-bluetooth-wi-fi-connected-wireless-speaker/soft-grey/p3645548"
+TAG_NAME = "p"
+QUERY = {"class": "price price--large"}
+
+request = requests.get(URL)
+content = request.content
+soup = BeautifulSoup(content, "html.parser")
+element = soup.find(TAG_NAME, QUERY)
+string_price = element.text.strip()
+
+print(string_price)
