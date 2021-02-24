72ef9f06e50106a0a497422a4888bfa65333ab70
vudinhquang <vudinhquangk53@gmail.com>
2021-02-24 15:17:24 +0900

    Creating the User model for our application

--- web_blog/src/models/user.py
+++ web_blog/src/models/user.py
@@ -1,4 +1,24 @@
 
 
 class User(object):
-    pass
+    def __init__(self):
+        pass
+    
+    def get_by_email(self):
+        pass
+
+    def get_by_id(self):
+        pass
+
+    def login_valid(self):
+        pass
+    
+    def register(self):
+        pass
+
+    def login(self):
+        pass
+
+    def get_blogs(self):
+        pass
+    
