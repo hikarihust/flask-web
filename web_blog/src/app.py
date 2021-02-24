20d2a3b7fcfaf7b84248e3ce7bf1c4c811afcc58
vudinhquang <vudinhquangk53@gmail.com>
2021-02-24 18:31:44 +0900

    The user registration page

--- web_blog/src/app.py
+++ web_blog/src/app.py
@@ -8,12 +8,16 @@ app = Flask(__name__)  # '__main__'
 app.secret_key = "jose"
 
 @app.route('/')
-def hello_method():
-    return "Hello, world!"
+def home_template():
+    return render_template('home.html')
 
 @app.route('/login')
 def login_template():
     return render_template('login.html')
+
+@app.route('/register')
+def register_template():
+    return render_template('register.html')
 
 @app.before_first_request
 def initialize_database():
@@ -28,6 +32,15 @@ def login_user():
         User.login(email)
     else:
         session['email'] = None
+
+    return render_template("profile.html", email=session['email'])
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
 
     return render_template("profile.html", email=session['email'])
 
