6ef0e54457ad14fdb02fa1d23f9af47b9cfa6c84
vudinhquang <vudinhquangk53@gmail.com>
2021-02-24 23:53:23 +0900

    Finalising the application by allowing to create posts!

--- web_blog/src/app.py
+++ web_blog/src/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, session, make_response
 from common.database import Database
 from models.user import User
 from models.blog import Blog
+from models.post import Post
 
 
 app = Flask(__name__)  # '__main__'
@@ -70,13 +71,27 @@ def create_new_blog():
         new_blog.save_to_mongo()
 
         return make_response(user_blogs(user._id))
-    
+
 @app.route('/posts/<string:blog_id>')
 def blog_posts(blog_id):
     blog = Blog.from_mongo(blog_id)
     posts = blog.get_posts()
 
     return render_template('posts.html', posts=posts, blog_title=blog.title, blog_id=blog._id)
+
+@app.route('/posts/new/<string:blog_id>', methods=['POST', 'GET'])
+def create_new_post(blog_id):
+    if request.method == 'GET':
+        return render_template('new_post.html', blog_id=blog_id)
+    else:
+        title = request.form['title']
+        content = request.form['content']
+        user = User.get_by_email(session['email'])
+
+        new_post = Post(blog_id, title, content, user.email)
+        new_post.save_to_mongo()
+
+        return make_response(blog_posts(blog_id))
 
 if __name__ == '__main__':
     app.run(port=4995, debug=True)
