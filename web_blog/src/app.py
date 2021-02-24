d6ee31e0b73a6bd675ea20e44e2359895b6d0059
vudinhquang <vudinhquangk53@gmail.com>
2021-02-24 17:29:32 +0900

    Our first template in Jinja2

--- web_blog/src/app.py
+++ web_blog/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)  # '__main__'
@@ -7,6 +7,10 @@ app = Flask(__name__)  # '__main__'
 @app.route('/')
 def hello_method():
     return "Hello, world!"
+
+@app.route('/login')
+def login_template():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
