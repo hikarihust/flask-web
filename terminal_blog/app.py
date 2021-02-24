71877c7bca2a65f4fa821b14c2c1222e6e8c1b63
vudinhquang <vudinhquangk53@gmail.com>
2021-02-24 11:43:14 +0900

    Verifying the Blog methods all work

--- terminal_blog/app.py
+++ terminal_blog/app.py
@@ -1,5 +1,16 @@
 from database import Database
-from models.post import Post
+from models.blog import Blog
 
 
 Database.initialize()
+
+blog=Blog(
+        author="Jose",
+        title="Sample title",
+        description="Sample description"
+    )
+
+blog.new_post()
+blog.save_to_mongo()
+from_database=Blog.from_mongo(blog.id)
+print(blog.get_posts())
