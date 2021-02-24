38006b727c85b0dbd797d84cd488e268ef624774
vudinhquang <vudinhquangk53@gmail.com>
2021-02-24 11:29:24 +0900

    The Blog class and @classmethods

--- terminal_blog/app.py
+++ terminal_blog/app.py
@@ -3,12 +3,3 @@ from models.post import Post
 
 
 Database.initialize()
-
-post = Post(blog_id="123",
-            title="Another great post",
-            content="This is some sample content",
-            author="jose"
-            )
-
-post.from_mongo('965ae76cb1364f60a62904ca48ea106f')
-print(post.json())
