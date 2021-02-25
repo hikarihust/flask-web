691d748993bfb9a2b9c50b8c6fbf7ff9094df20e
vudinhquang <vudinhquangk53@gmail.com>
2021-02-25 11:29:18 +0900

    Preparing our Item model for saving in MongoDB

--- web-stores/models/item.py
+++ web-stores/models/item.py
@@ -2,15 +2,21 @@ from typing import Dict
 from bs4 import BeautifulSoup
 import requests
 import re
+import uuid
 
 
 class Item:
-    def __init__(self, url: str, tag_name: str, query: Dict):
+    def __init__(self, url: str, tag_name: str, query: Dict, _id: str = None):
         self.url = url
         self.tag_name = tag_name
         self.query = query
         self.price = None
+        self.collection = "items"
+        self._id = _id or uuid.uuid4().hex
 
+    def __repr__(self):
+        return f"<Item {self.url}>"
+    
     def load_price(self) -> float:
         request = requests.get(self.url)
         content = request.content
@@ -24,4 +30,12 @@ class Item:
         without_commas = found_price.replace(",", "")
         self.price = float(without_commas)
         return self.price
+    
+    def json(self) -> Dict:
+        return {
+            "_id": self._id,
+            "url": self.url,
+            "tag_name": self.tag_name,
+            "query": self.query
+        }
     
