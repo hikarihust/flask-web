fd429411bef5812d087d6ed014cac4eba97e687e
vudinhquang <vudinhquangk53@gmail.com>
2021-02-26 01:29:39 +0900

    Creating items through the web interface

--- web-stores/app.py
+++ web-stores/app.py
@@ -1,14 +1,21 @@
+import json
+from flask import Flask, render_template, request
+
 from models.item import Item
-from models.alert import Alert
 
 
-ipad = Item(
-    "https://www.johnlewis.com/2020-apple-ipad-pro-11-inch-a12z-bionic-ios-wi-fi-1tb/space-grey/p4949061",
-    "p",
-    {"class": "price price--large"}
-)
+app = Flask(__name__)
 
-ipad.save_to_mongo()
+@app.route('/', methods=['GET', 'POST'])
+def create_item():
+    if request.method == 'POST':
+        url = request.form['url']
+        tag_name = request.form['tag_name']
+        query = json.loads(request.form['query'])
 
-alert = Alert(ipad._id, 2000)
-alert.save_to_mongo()
+        Item(url, tag_name, query).save_to_mongo()
+
+    return render_template("items/new_item.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
