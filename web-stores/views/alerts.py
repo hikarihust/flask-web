f8ba8015e82309f9eadbf256000474ff5eda37b6
vudinhquang <vudinhquangk53@gmail.com>
2021-02-26 12:11:25 +0900

    Testing app with stores

--- web-stores/views/alerts.py
+++ web-stores/views/alerts.py
@@ -16,12 +16,12 @@ def index():
 def create_alert():
     if request.method == 'POST':
         item_url = request.form["item_url"]
-        
+
         store = Store.find_by_url(item_url)
         item = Item(item_url, store.tag_name, store.query)
         item.save_to_mongo()
 
-        price_limit = request.form['price_limit']
+        price_limit = float(request.form["price_limit"])
         Alert(item._id, price_limit).save_to_mongo()
     # What happens if it's a GET request
     return render_template("alerts/new_alert.html")
