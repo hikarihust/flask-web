e6b7dba9429ca7a64d78badbe81fc25e5949db53
vudinhquang <vudinhquangk53@gmail.com>
2021-02-27 01:06:27 +0900

    Editing alert properties

--- web-stores/views/alerts.py
+++ web-stores/views/alerts.py
@@ -28,3 +28,18 @@ def create_alert():
     # What happens if it's a GET request
     return render_template("alerts/new_alert.html")
     
+
+@alert_blueprint.route('/edit/<string:alert_id>', methods=['GET', 'POST'])
+def edit_alert(alert_id):
+    if request.method == 'POST':
+        price_limit = float(request.form['price_limit'])
+
+        alert = Alert.get_by_id(alert_id)
+        alert.price_limit = price_limit
+        alert.save_to_mongo()
+
+        return redirect(url_for('.index'))
+
+    # What happens if it's a GET request
+    return render_template("alerts/edit_alert.html", alert=Alert.get_by_id(alert_id)) 
+    
