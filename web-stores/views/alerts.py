cdf5440a5205a619ecb27412e14b2975431ab8a9
vudinhquang <vudinhquangk53@gmail.com>
2021-03-01 01:29:05 +0900

    Limiting some pages only to logged in users

--- web-stores/views/alerts.py
+++ web-stores/views/alerts.py
@@ -3,16 +3,19 @@ from flask import Blueprint, render_template, request, redirect, url_for, sessio
 from models.alert import Alert
 from models.store import Store
 from models.item import Item
+from models.user import requires_login
 
 
 alert_blueprint = Blueprint('alerts', __name__)
 
 @alert_blueprint.route('/')
+@requires_login
 def index():
     alerts = Alert.find_many_by('user_email', session['email'])
     return render_template('alerts/index.html', alerts=alerts)
 
 @alert_blueprint.route('/new', methods=['GET', 'POST'])
+@requires_login
 def create_alert():
     if request.method == 'POST':
         item_url = request.form["item_url"]
@@ -30,6 +33,7 @@ def create_alert():
     
 
 @alert_blueprint.route('/edit/<string:alert_id>', methods=['GET', 'POST'])
+@requires_login
 def edit_alert(alert_id):
     if request.method == 'POST':
         price_limit = float(request.form['price_limit'])
@@ -45,6 +49,7 @@ def edit_alert(alert_id):
     
 
 @alert_blueprint.route('/delete/<string:alert_id>')
+@requires_login
 def delete_alert(alert_id):
     alert = Alert.get_by_id(alert_id)
     if alert.user_email == session['email']:
