8f2ebde0a0eeac450eada5cb17270ed222e54076
vudinhquang <vudinhquangk53@gmail.com>
2021-03-02 00:35:15 +0900

    Limiting actions to admins only

--- web-stores/views/stores.py
+++ web-stores/views/stores.py
@@ -2,17 +2,20 @@ import json
 from flask import Blueprint, render_template, request, redirect, url_for
 
 from models.store import Store
+from models.user import requires_login, requires_admin
 
 
 store_blueprint = Blueprint('stores', __name__)
 
 @store_blueprint.route('/')
+@requires_login
 def index():
     stores = Store.all()
     return render_template('stores/store_index.html', stores=stores)
 
 
 @store_blueprint.route('/new', methods=['GET', 'POST'])
+@requires_admin
 def create_store():
     if request.method == 'POST':
         name = request.form['name']
@@ -27,6 +30,7 @@ def create_store():
 
 
 @store_blueprint.route('/edit/<string:store_id>', methods=['GET', 'POST'])
+@requires_admin
 def edit_store(store_id):
     if request.method == 'POST':
         name = request.form['name']
@@ -50,6 +54,7 @@ def edit_store(store_id):
     
 
 @store_blueprint.route('/delete/<string:store_id>')
+@requires_admin
 def delete_store(store_id):
     Store.get_by_id(store_id).remove_from_mongo()
     return redirect(url_for('.index'))
