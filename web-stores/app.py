46ab606d18d3fa6d274d85c920f086b4d7b2499a
vudinhquang <vudinhquangk53@gmail.com>
2021-03-02 17:33:58 +0900

    Creating the landing page

--- web-stores/app.py
+++ web-stores/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 from views.alerts import alert_blueprint
 from views.stores import store_blueprint
@@ -15,6 +15,10 @@ app.config.update(
 app.register_blueprint(alert_blueprint, url_prefix="/alerts")
 app.register_blueprint(store_blueprint, url_prefix="/stores")
 app.register_blueprint(user_blueprint, url_prefix="/users")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
